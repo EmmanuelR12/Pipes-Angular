a74ebd088e2d1b117cd4254203e10803277db22c
EmmanuelR12 <erangel@tecnolika.com>
2022-05-20 16:18:12 -0500

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre:string= 'Emmanuel rangel'
+
+  mostrarNombre(){
+    console.log(this.nombre);
+    
+  }
 }
